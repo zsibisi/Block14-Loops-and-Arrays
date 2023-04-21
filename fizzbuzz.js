a2ae7db62602ade2951c83e720f31784b81d5dda
Zinhle Sibisi <107902020+zsibisi@users.noreply.github.com>
2023-04-21 07:21:33 -0700

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,24 +1,28 @@
-// Define a loop that will run from 1 to 100
+// This code is defining a loop that will run from 1 to 100 using a for loop.
 for (let i = 1; i <= 100; i++) {
-// Check if the current number is a multiple of both 3 and 5 using the modulus operator
+// Within the loop, the code is checking if the current number is a multiple of both 3 and 5 using the modulus operator.
 if (i % 3 === 0 && i % 5 === 0) {
-// If the number is a multiple of both 3 and 5, print "FizzBuzz"
+// If the current number is a multiple of both 3 and 5, the code prints "FizzBuzz"
 console.log("FizzBuzz");
 }
-// Check if the current number is a multiple of 3 using the modulus operator
+// If the current number is not a multiple of both 3 and 5, the code checks if the current number is a multiple of 3 using the modulus operator.
 else if (i % 3 === 0) {
-// If the number is a multiple of 3, print "Fizz"
+// If the current number is a multiple of 3, the code prints "Fizz"
 console.log("Fizz");
 }
-// Check if the current number is a multiple of 5 using the modulus operator
+// If the current number is not a multiple of both 3 and 5 or just 3, the code checks if the current number is a multiple of 5 using the modulus operator.
 else if (i % 5 === 0) {
-// If the number is a multiple of 5, print "Buzz"
+// If the current number is a multiple of 5, the code prints "Buzz"
 console.log("Buzz");
 }
-// If the number is not a multiple of 3 or 5, print the number itself
+// If the current number is not a multiple of both 3 and 5, just 3 or just 5, the code prints the current number.
 else {
 console.log(i);
 }
 }
+
+
+
+
 
 
