9f4f6420d870e2c0263086032d3b00a1ba234dc5
Zinhle Sibisi <107902020+zsibisi@users.noreply.github.com>
2023-04-21 07:03:03 -0700

    Update reverse-array.js

---
+++ reverse-array.js
@@ -0,0 +1,9 @@
+// creating a new array numbers to be reversed / Declaring an array.
+let numbers = [20,50,30,60,200];
+
+// Use the reverse() method to reverse the array in place.
+numbers.reverse()
+
+
+// Output the original and reversed arrays
+console.log(numbers);
