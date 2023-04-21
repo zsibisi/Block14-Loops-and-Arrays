f0c7b2872a92d29a3f27796b1c2a27a3c97e3061
Zinhle Sibisi <107902020+zsibisi@users.noreply.github.com>
2023-04-21 07:17:21 -0700

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,24 @@
-// loop through numbers from 1 to 100
+// Define a loop that will run from 1 to 100
 for (let i = 1; i <= 100; i++) {
-    // check if the number is a multiple of both 3 and 5
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
-    } 
-    // check if the number is a multiple of 3
-    else if (i % 3 === 0) {
-      console.log("Fizz");
-    } 
-    // check if the number is a multiple of 5
-    else if (i % 5 === 0) {
-      console.log("Buzz");
-    } 
-    // if none of the above, just print the number
-    else {
-      console.log(i);
-    }
-  }
-  
-.
+// Check if the current number is a multiple of both 3 and 5 using the modulus operator
+if (i % 3 === 0 && i % 5 === 0) {
+// If the number is a multiple of both 3 and 5, print "FizzBuzz"
+console.log("FizzBuzz");
+}
+// Check if the current number is a multiple of 3 using the modulus operator
+else if (i % 3 === 0) {
+// If the number is a multiple of 3, print "Fizz"
+console.log("Fizz");
+}
+// Check if the current number is a multiple of 5 using the modulus operator
+else if (i % 5 === 0) {
+// If the number is a multiple of 5, print "Buzz"
+console.log("Buzz");
+}
+// If the number is not a multiple of 3 or 5, print the number itself
+else {
+console.log(i);
+}
+}
+
+
